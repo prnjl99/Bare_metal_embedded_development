c9077512e88e9bbcf8a2c35e9e4c44c16d714c96
Pranjal Arya <pranjalarya9999@gmail.com>
2023-01-26 22:54:17 +0530

    Create makefile.yml (#4)
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>
    
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -2,10 +2,18 @@
 # Compile target application
 
 # Fetch ROOT directory path
-ROOT_FOLDER="$(cygpath --mixed "$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)")"
+ROOT_FOLDER="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+case `uname` in
+    *CYGWIN*|*MINGW*|*MSYS*) ROOT_FOLDER=`cygpath -w "$ROOT_FOLDER"`;;
+esac
+
+if [[ ! -v CI ]];then
+    export CI=false
+fi
 
 if [[ $1 == build ]];then
-    make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER all
+    make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI all
 elif [[ $1 == clean ]];then
     find . -name '*.o' -exec rm -r {} \;
 fi
