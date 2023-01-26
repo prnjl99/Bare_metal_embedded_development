1e127ad4a6c8750637bba849ab0daed8e64689d0
Pranjal Arya <pranjalarya9999@gmail.com>
2023-01-26 16:01:08 +0530

    Add working build setup (#3)
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>
    
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-ROOT_FOLDER=$(dirname ${BASH_SOURCE[0]})
-
 # Compile target application
-make APP=$1 ROOT_FOLDER=$ROOT_FOLDER clean
+
+# Fetch ROOT directory path
+ROOT_FOLDER="$(cygpath --mixed "$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)")"
+
+if [[ $1 == build ]];then
+    make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER all
+elif [[ $1 == clean ]];then
+    find . -name '*.o' -exec rm -r {} \;
+fi
