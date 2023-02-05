bc74b0638fc7d949f73fa9bc7ac3d29c533e7e05
Pranjal Arya <pranjalarya9999@gmail.com>
2023-02-05 22:54:03 +0530

    Generate bootable executable file (#10)
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- bsp/startup.c
+++ bsp/startup.c
@@ -1,1 +1,25 @@
-int first=0;
+#include "startup.h"
+
+extern int main(void);
+extern void _initialStackPoniter(void);
+
+__attribute__((naked, noreturn)) void _resetHandler(void)
+{
+	extern long _startbss, _endbss, _startdata, _enddata, _absaddrdata;
+	for(long *src = &_startbss; src < &_endbss; src++)
+	{
+		*src = 0;
+	}
+    for(long *src = &_startdata, *dst = &_absaddrdata; src < &_enddata;)
+	{
+		*src++ = *dst++;
+	}
+	main();
+	while(1){};
+}
+
+__attribute__((section(".vtors"))) void (*vectorTable[STANDARD_INTR_COUNT + USER_INTR_COUNT])(void)=
+{
+    _initialStackPoniter,
+	_resetHandler
+};
