ca4636f63b628cf60125b9335d8d41ab74e667fe
Pranjal Arya <pranjalarya9999@gmail.com>
2023-02-09 22:49:34 +0530

    Modify project to build blinky image
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -5,7 +5,7 @@
 ROOT_FOLDER="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 case `uname` in
-    *CYGWIN*|*MINGW*|*MSYS*) ROOT_FOLDER=`cygpath -w "$ROOT_FOLDER"`;;
+    *CYGWIN*|*MINGW*|*MSYS*) ROOT_FOLDER=`cygpath -m "$ROOT_FOLDER"`;;
 esac
 
 if [[ ! -v CI ]];then
@@ -14,7 +14,12 @@ fi
 
 if [[ $1 == build ]];then
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI all
+elif [[ $1 == flash ]];then
+    make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI flash
+elif [[ $1 == disass ]];then
+    make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI FUNC=$3 disass
 elif [[ $1 == clean ]];then
     find . -name '*.o' -exec rm -r {} \;
+    find . -name '*.d' -exec rm -r {} \;
     rm -rf "$ROOT_FOLDER/test_applications/build"
 fi
