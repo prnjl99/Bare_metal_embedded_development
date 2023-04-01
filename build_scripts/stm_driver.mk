52309552c1ff3bf279f14da26b9da87826079034
Pranjal Arya <pranjalarya9999@gmail.com>
2023-04-01 14:05:54 +0530

    Reorganize makefiles
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/stm_driver.mk
+++ build_scripts/stm_driver.mk
@@ -1,15 +1,6 @@
 # Makefile to build target application from <ROOT>/test_applications
-ifeq ($(CI),true)
-    CC:=gcc
-    CFLAGS=-Wno-pointer-to-int-cast -DCI_ENABLED
-else
-    CC:=arm-none-eabi-gcc
-    LD:=arm-none-eabi-ld
-    AS:=arm-none-eabi-as
-    AR:=arm-none-eabi-ar
-    OBJCOPY:=arm-none-eabi-objcopy
-    GDB:=arm-none-eabi-gdb
-    READELF:=arm-none-eabi-readelf
+ifneq ($(CI),true)
+    LD:=arm-none-eabi-gcc
 endif
 
 APP?=uart
@@ -23,7 +14,8 @@ ifneq ($(CI),true)
     AFLAGS+=-Wall -mlittle-endian -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -g3
 endif
 
-LFLAGS=-T $(ROOT_FOLDER)/stm_files/bsp/STM32L476RGTX_FLASH.ld -mcpu=cortex-m4 --specs=nosys.specs -Wl,-Map="$(OBJDIR)/$(APP).map" -Wl,--gc-sections -static --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -Wl,--start-group -lc -lm -Wl,--end-group
+LFLAGS=-T $(ROOT_FOLDER)/stm_files/bsp/STM32L476RGTX_FLASH.ld -mcpu=cortex-m4 --specs=nosys.specs -Wl,-Map="$(OBJDIR)/$(APP).map"
+LFLAGS+=-Wl,--gc-sections -static --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -Wl,--start-group -lc -lm -Wl,--end-group
 
 INCLUDES=-I$(ROOT_FOLDER)/stm_files/driver/BSP/STM32L4xx_Nucleo -I$(ROOT_FOLDER)/stm_files/driver/CMSIS/Device/ST/STM32L4xx/Include
 INCLUDES+=-I$(ROOT_FOLDER)/stm_files/driver/CMSIS/Include -I$(ROOT_FOLDER)/stm_files/driver/STM32L4xx_HAL_Driver/Inc
@@ -39,73 +31,3 @@ ASSM+=$(wildcard $(ROOT_FOLDER)/stm_files/bsp/*.s)
 LDFILES=$(wildcard $(ROOT_FOLDER)/stm_files/bsp/*.ld)
 
 ARCH_FILE=$(wildcard $(ROOT_FOLDER)/stm_files/bsp/*.a)
-
-OBJS=$(SRCS:.c=.o) $(ASSM:.s=.o)
-
-$(info ######################################################)
-$(info compiling sources)
-$(info ######################################################)
-
-CREATE_DIRS:
-	@mkdir -p $(OBJDIR)
-	@mkdir -p $(APPDIR)
-
-MV_FILES_BUILD:
-	@mv $(OBJS) $(OBJDIR)
-
-.PHONY: depend clean all
-
-all:	MAINFUNC GENBIN MV_FILES_BUILD
-		$(info ######################################################)
-		$(info $(APP) built successfully)
-		$(info ######################################################)
-
-flash:	all
-		openocd -f stm.cfg -c "init; reset halt; stm32l4x mass_erase 0; exit"
-		openocd -f stm.cfg -c "program $(APPDIR)/$(APP).bin reset exit 0x08000000"
-
-debug:	all
-		openocd -f stm.cfg -c "init; reset init"
-
-erase:
-		openocd -f stm.cfg -c "init; reset halt; stm32l4x mass_erase 0; exit"
-
-# For debugging with GDB:
-# 		openocd -f stm.cfg -c "init; reset init"
-#  		Then start GDB in other shell and execute following commands:
-#		target remote :3333
-#		monitor reset init
-#		file <PATH TO .elf FILE>
-#		load
-# Can be programmed via st-flash:
-#	 st-flash --reset write $(APPDIR)/$(APP).bin 0x8000000
-
-MAINFUNC:$(OBJS) CREATE_DIRS
-
-%.o:%.c
-		$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
-
-%.o:%.s
-		$(CC) $(AFLAGS) $(INCLUDES) -c $< -o $@
-
-GENELF:
-		$(info ######################################################)
-		$(info linking object files)
-		$(info ######################################################)
-		$(CC) $(LFLAGS) $(OBJS) -o $(APPDIR)/$(APP).elf
-		$(READELF) -Sl $(APPDIR)/$(APP).elf > $(APPDIR)/$(APP).readelf
-
-GENBIN:GENELF
-		$(info ######################################################)
-		$(info generating bin file)
-		$(info ######################################################)
-		$(OBJCOPY) -O binary $(APPDIR)/$(APP).elf $(APPDIR)/$(APP).bin
-
-depend: $(SRCS)
-		makedepend $(INCLUDES) $^
-
-disass: all
-		$(GDB) $(APPDIR)/$(APP).elf -batch -ex 'disass /r $(FUNC)'
-
-clean:
-		rm -rf $(OBJS) $(APPDIR)
