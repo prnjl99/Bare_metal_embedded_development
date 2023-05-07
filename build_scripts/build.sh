add63159ba87c9d962b152a9faa7f5b64e108b87
Pranjal Arya <pranjalarya9999@gmail.com>
2023-05-07 11:16:09 +0530

    Fetch dependencies
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -18,13 +18,28 @@ if [[ ! -v CI ]];then
     export CI=false
 fi
 
+# Use this to zip directories
+#zip -r $ROOT_FOLDER/stm_files.zip $ROOT_FOLDER/stm_files/
+#exit 1;
+
+get_dep()
+{
+	if [[ ! -d $ROOT_FOLDER/stm_files ]]; then
+		unzip $ROOT_FOLDER/stm_files.zip
+		rm -rf $ROOT_FOLDER/C:
+	fi
+}
+
 if [[ $1 == build ]];then
+	get_dep
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI stm_build=$stm_build all
 elif [[ $1 == erase ]];then
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI stm_build=$stm_build erase
 elif [[ $1 == flash ]];then
+	get_dep
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI stm_build=$stm_build flash
 elif [[ $1 == debug ]];then
+	get_dep
 	if [[ ! -f $ROOT_FOLDER/test_applications/build/$2/$2.elf ]]; then
 		make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI stm_build=$stm_build all
 	fi
@@ -37,6 +52,7 @@ elif [[ $1 == debug ]];then
     -ex "load"
 #    -ex "source -v $ROOT_FOLDER/build_scripts/gdb_cmd"
 elif [[ $1 == disass ]];then
+	get_dep
     make --no-print-directory -C $ROOT_FOLDER/build_scripts APP=$2 ROOT_FOLDER=$ROOT_FOLDER CI=$CI FUNC=$3 stm_build=$stm_build disass
 elif [[ $1 == clean ]];then
     find . -name '*.o' -exec rm -r {} \;
