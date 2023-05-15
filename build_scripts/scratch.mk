1ed06fea1999722b0c4af0d574a80edac158e51f
Pranjal Arya <pranjalarya9999@gmail.com>
2023-05-15 22:21:56 +0530

    Add support for dynamic memory allocation ( heap )
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- build_scripts/scratch.mk
+++ build_scripts/scratch.mk
@@ -1,6 +1,6 @@
 # Makefile to build target application from <ROOT>/test_applications
 ifneq ($(CI),true)
-    LD:=arm-none-eabi-ld
+    LD:=arm-none-eabi-gcc
 endif
 
 APP?=first_app
@@ -10,15 +10,19 @@ OBJDIR=$(ROOT_FOLDER)/test_applications/build/$(APP)/generated_files
 CFLAGS+=-Wall -Wextra -Werror
 
 ifneq ($(CI),true)
-    CFLAGS+=-g -mlittle-endian -mthumb -mcpu=cortex-m4
-    CFLAGS+=-mfloat-abi=hard -mfpu=fpv4-sp-d16 --specs=nosys.specs
+    CFLAGS+=-g3 -O0 -mlittle-endian -mthumb -mcpu=cortex-m4 --specs=nosys.specs
+    CFLAGS+=-mfloat-abi=hard -mfpu=fpv4-sp-d16 --specs=nano.specs -std=gnu11
     # Generate separate sections for each data functions
     CFLAGS+=-fdata-sections -ffunction-sections
 endif
 
-LFLAGS=-T $(ROOT_FOLDER)/bsp/linker.ld -Map=$(APPDIR)/$(APP).map
-# Enable this to discard sections using garbage collector
-#LFLAGS+=--gc-sections
+LFLAGS=-T $(ROOT_FOLDER)/bsp/linker.ld
+ifneq ($(CI),true)
+    LFLAGS+=-mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mthumb --specs=nano.specs --specs=nosys.specs
+    LFLAGS+=-static -Wl,--start-group -lc -lm -Wl,--end-group -Wl,-Map=$(APPDIR)/$(APP).map 
+    # Enable this to discard sections using garbage collector
+    #LFLAGS+=--gc-sections
+endif
 
 INCLUDES=-I$(ROOT_FOLDER)/driver/headers -I$(ROOT_FOLDER)/bsp -I$(ROOT_FOLDER)/test_applications/scratch/$(APP)
 
