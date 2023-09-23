bce53c7b5f636b995f988055da553fb8c6d1e12e
Pranjal Arya <pranjalarya9999@gmail.com>
2023-09-23 16:10:30 +0530

    Heap and stack analysis
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- test_applications/scratch/stm/heap_stack/main.c
+++ test_applications/scratch/stm/heap_stack/main.c
@@ -32,12 +32,57 @@ void *_sbrk(size_t incr)
   return temp;
 }
 
+/*#define find_size(arg) ((arg)
+
+find_size_func(*/
+
 int main(void)
 {
-    int *ptr = (int*)malloc(sizeof(int));
-    int *ptr1 = (int*)malloc(100*sizeof(int));
+	int size=sizeof(int);
+	int size1=sizeof(size);
+    int *ptr1 = (int*)malloc(size);
+    int *ptr2 = (int*)malloc(size);
+    int *ptr3 = (int*)malloc(size);
+    int *ptr4 = (int*)malloc(size);
+    int *ptr5 = (int*)malloc(size);
+    int *ptr6 = (int*)malloc(size);
+    int *ptr7 = (int*)malloc(size);
+    int *ptr8 = (int*)malloc(size);
+    int *ptr9 = (int*)malloc(size);
+    int *ptr10 = (int*)malloc(size);
+    int *ptr11 = (int*)malloc(size);
+    int *ptr12 = (int*)malloc(size);
+    int *ptr13 = (int*)malloc(size);
+    int *ptr14 = (int*)malloc(size);
+    int *ptr15 = (int*)malloc(size);
+    int *ptr16 = (int*)malloc(size);
+    int *ptr17 = (int*)malloc(size);
+    int *ptr18 = (int*)malloc(size);
+    int *ptr19 = (int*)malloc(size);
+    int *ptr20 = (int*)malloc(size1);
 
-    if((ptr != NULL) && (ptr1 == NULL))
+	*ptr1 = 0x12345678;
+	*ptr2 = 0x87654321;
+	*ptr3 = 0x12345678;
+	*ptr4 = 0x87654321;
+	*ptr5 = 0x12345678;
+	*ptr6 = 0x87654321;
+	*ptr7 = 0x12345678;
+	*ptr8 = 0x87654321;
+	*ptr9 = 0x12345678;
+	*ptr10 = 0x87654321;
+	*ptr11 = 0x12345678;
+	*ptr12 = 0x87654321;
+	*ptr13 = 0x12345678;
+	*ptr14 = 0x87654321;
+	*ptr15 = 0x12345678;
+	*ptr16 = 0x87654321;
+	*ptr17 = 0x12345678;
+	*ptr18 = 0x87654321;
+	*ptr19 = 0x12345678;
+	*ptr20 = 0x87654321;
+
+    if((ptr20 != NULL) && (ptr19 != NULL) && (ptr18 != NULL) && (ptr17 != NULL) && (ptr16 != NULL) && (ptr15 != NULL) && (ptr14 != NULL) && (ptr13 != NULL) && (ptr12 != NULL) && (ptr11 != NULL) && (ptr10 != NULL) && (ptr9 != NULL) && (ptr8 != NULL) && (ptr7 != NULL) && (ptr6 != NULL) && (ptr5 != NULL) && (ptr4 != NULL) && (ptr3 != NULL) && (ptr2 != NULL) && (ptr1 != NULL))
     {
         toggle_LED();
     }
--- test_applications/scratch/stm/toggle_led/main.c
+++ test_applications/scratch/stm/toggle_led/main.c
@@ -1,6 +1,7 @@
 #include "startup.h"
 #include "common.h"
 #include "gpio.h"
+#include <stdio.h>
 
 const uint32_t pra_const = 1UL;
 char *pra_arr="Hello";
@@ -8,7 +9,10 @@ char pra_arr_1[]="Hello";
 
 int main(void)
 {
+    volatile float test_f = 1.035;
     toggle_LED();
+	printf("%f",test_f);
+	(void)test_f;
 	(void)pra_const;
 	(void)pra_arr;
 	(void)pra_arr_1;
